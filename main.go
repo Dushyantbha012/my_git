90c71aa13e0def9d2b67c45a0ef485c1a56ada9a
Dushyant Bhardwaj <dushyantbhardwaj@AirStalin.local>
2024-12-05 23:56:39 +0530

    implemented cat-file

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"fmt"
 	"os"
+	"path"
+	"strings"
 )
 
 // Usage: your_program.sh <command> <arg1> <arg2> ...
@@ -14,6 +18,8 @@ func main() {
 	}
 
 	switch command := os.Args[1]; command {
+	case "usage:":
+		fmt.Fprintf(os.Stderr, "will implement it\n")
 	case "init":
 		//Uncomment this block to pass the first stage!
 
@@ -29,7 +35,44 @@ func main() {
 		}
 
 		fmt.Println("Initialized git directory")
-
+	case "cat-file":
+		if len(os.Args) < 4 {
+			fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
+			os.Exit(1)
+		}
+		if os.Args[2] != "-p" {
+			fmt.Fprintf(os.Stderr, "error, second params needs to be '-p'\n")
+			os.Exit(1)
+		}
+		dirname := os.Args[3][:2]
+		filename := os.Args[3][2:]
+		objectPath := path.Join(".git", "objects", dirname, filename)
+		fileBytes, err := os.ReadFile(objectPath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "couldn't read the file")
+			os.Exit(1)
+		}
+		bytesReader := bytes.NewReader(fileBytes)
+		r, err := zlib.NewReader(bytesReader)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error creating zlib new reader")
+			os.Exit(1)
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "couldn't read the file")
+			os.Exit(1)
+		}
+		defer r.Close()
+		var decompressedData bytes.Buffer
+		_, err = decompressedData.ReadFrom(r)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error decompressing data")
+			os.Exit(1)
+		}
+		resultString := decompressedData.String()
+		splits := strings.SplitN(resultString, "\x00", 2)
+		_, content := splits[0], splits[1]
+		fmt.Print(content)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
